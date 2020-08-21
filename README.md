cbcb53f21a35241ba6d8fd7ba39f6b0574611c51
baozouai <baozouai@gmail.com>
2020-08-21 23:47:20 +0800

    Z 字形变换

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
+<!--
+ * @Description: 
+ * @Author: Moriaty
+ * @Date: 2020-08-18 09:22:14
+ * @Last modified by: Moriaty
+ * @LastEditTime: 2020-08-21 23:46:34
+-->
 # 使用ts刷leecode
 
 ## 数组
 
 |                             标题                             | 难度 |
 | :----------------------------------------------------------: | :--: |
-|   [两个数组交集](./src/01Array/two-array-intersection.ts)    |  ⭐️   |
-|    [最长公共前缀](./src/01Array/longest-common-peifix.ts)    |  ⭐️   |
-| [买卖股票最佳时机II](./src/01Array/best-time-to-buy-and-sell-stock-ii.ts) |  ⭐️   |
-| [翻转数组](./src/01Array/rotate-array.ts) |  ⭐️   |
-| [加一](./src/01Array/plus-one.ts) |  ⭐️   |
-| [两数之和](./src/01Array/two-sum.ts) |  ⭐️   |
-| [三数之和为0](./src/01Array/3sum.ts) |  ⭐️⭐️   |
+|   [两个数组交集（350）](./src/01Array/two-array-intersection.ts)    |  ⭐️   |
+|    [最长公共前缀（14）](./src/01Array/longest-common-peifix.ts)    |  ⭐️   |
+| [买卖股票最佳时机II（122）](./src/01Array/best-time-to-buy-and-sell-stock-ii.ts) |  ⭐️   |
+| [翻转数组（189）](./src/01Array/rotate-array.ts) |  ⭐️   |
+| [加一（27）](./src/01Array/plus-one.ts) |  ⭐️   |
+| [两数之和（66）](./src/01Array/two-sum.ts) |  ⭐️   |
+| [三数之和为0（15）](./src/01Array/3sum.ts) |  ⭐️⭐️   |
+| [Z 字形变换（6）](./src/01Array/zigzag-conversion.ts) |  ⭐️⭐️   |
 
---
+++ src/01Array/zigzag-conversion.ts
@@ -0,0 +1,79 @@
+/*
+* @Description: 
+*    将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+*    比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+*   
+*   L   C   I   R
+*   E T O E S I I G
+*   E   D   H   N
+*
+*   之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+*   
+*   请你实现这个将字符串进行指定行数变换的函数：
+*   
+*   string convert(string s, int numRows);
+* @Author: Moriaty
+* @Date: 2020-08-21 23:39:05
+* @Last modified by: Moriaty
+ * @LastEditTime: 2020-08-21 23:47:04
+*/
+/**
+* 示例 1:
+*
+*    输入: s = "LEETCODEISHIRING", numRows = 3
+*    输出: "LCIRETOESIIGEDHN"
+* 
+* 示例 2:
+*    
+*    输入: s = "LEETCODEISHIRING", numRows = 4
+*    输出: "LDREOEIIECIHNTSG"
+*    解释:
+*    
+*    L     D     R
+*    E   O E   I I
+*    E C   I H   N
+*    T     S     G
+*    
+*  来源：力扣（LeetCode）
+*  链接：https://leetcode-cn.com/problems/zigzag-conversion
+*    
+*/
+
+/**
+ * @description: 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列
+ * @param {string}  s 给定字符串
+ * @param {number}  numRows 给定的行数
+ * @return {string}  新的字符串
+ */
+// 第一种方法
+function convert(s: string, numRows: number): string {
+    if (numRows === 1) return s;
+    const strArr: string[] = Array(numRows).fill('');
+    // 周期为2 * numRows - 2
+    const period: number = 2 * numRows - 2;
+    for (let i = 0; i < s.length; ++i) {
+        // 取模
+        const mod = i % period;
+        // 如果模小于numRows 则正向走（规定向下为正） 否则反向
+        const index = mod < numRows ? mod : period - mod;
+        strArr[index] += s[i];
+    }
+    // 第二种方法
+    return strArr.join('');
+};
+// 第二种方法
+function convert1(s: string, numRows: number): string {
+    if (numRows === 1) return s;
+    const strArr: string[] = Array(numRows).fill('');
+    let i = 0;
+    let flag = -1;
+    for (let j = 0; j < s.length; ++j) {
+        strArr[i] += s[j];
+        // 到头和尾加完就转向
+        if (i === 0 || i === numRows - 1) flag = -flag;
+        i += flag;
+    }
+    return strArr.join('');
+};
+
+console.log(convert("PAYPALISHIRING", 3));
