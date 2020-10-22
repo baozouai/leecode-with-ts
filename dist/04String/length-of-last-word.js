89ee7a59bbe4c384e65e21e3edc99603b6a046b6
baozouai <baozouai@gmail.com>
2020-10-22 23:26:38 +0800

    最后一个单词的长度修改注释

--- dist/04String/length-of-last-word.js
+++ dist/04String/length-of-last-word.js
@@ -4,7 +4,7 @@
  * @Author: Moriaty
  * @Date: 2020-09-05 20:40:51
  * @Last modified by: Moriaty
- * @LastEditTime: 2020-09-05 20:48:47
+ * @LastEditTime: 2020-10-22 23:22:40
  */
 /**
  * 示例 1:
@@ -14,12 +14,12 @@
  *    链接：https://leetcode-cn.com/problems/length-of-last-word
  */
 /**
- * @description: 给定两个字符串, A 和 B。A 的旋转操作就是将 A 最左边的字符移动到最右边。
- *               例如, 若 A = 'abcde'，在移动一次之后结果就是'bcdea' 。
- *               如果在若干次旋转操作之后，A 能变成B，那么返回True
- * @param {string[]} A 字符串
- * @param {string[]} B 字符串
- * @return {boolean} 若干次旋转操作之后，A是否等于B
+ * @description: 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+ *               如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+ *               如果不存在最后一个单词，请返回 0 。
+ *               说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串
+ * @param {string} s 字符串
+ * @return {number} 最后一个单词的长度
  */
 function lengthOfLastWord(s) {
     if (!s.match(/[a-zA-Z]/)) {
